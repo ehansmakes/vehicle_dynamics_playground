8552ee1761e807477411fc283e88c9901c6a333e
Erik Contreras <ehans1c@gmail.com>
2024-11-13 17:05:01 -0800

    Update g-h filter with sample data

--- data_filters/g-h_filter.py
+++ data_filters/g-h_filter.py
@@ -25,6 +25,7 @@ NOTE: "g is the scaling we use for the measurement"
 
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 
 def g_h_filter(data, x_0, dx, g, h, dt=1):
@@ -42,7 +43,7 @@ def g_h_filter(data, x_0, dx, g, h, dt=1):
         residual = z - x_pred 
         dx = dx + h * (residual) / dt
         x_est = x_pred + g * residual
-        results.append(round(x_est,1))
+        results.append(round(x_est,3))
     return np.array(results)
 
 
@@ -50,27 +51,33 @@ def g_h_filter(data, x_0, dx, g, h, dt=1):
 
 # UPLOAD DATA FROM CSV --------------------------------------------------------
 # under construction 
-'''
-with open("grades.csv") as new_file:
-    for line in new_file:
-        line = line.replace("\n", "")
-        parts = line.split(";")
-        name = parts[0]
-        grades = parts[1:]
-''' 
+
+
+#path = 'C:\Users\ehans\OneDrive\Documents\DARC Files\Scripts\vehicle_dynamics_playground\data_filters\test_data\sample_data_weight.csv'
+
+#with open(path, "r") as file:
+#   csv_reader = csv.reader(file)
+#   for row in csv_reader:
+#        print(row)
+
+    
 
 # GIVEN DATA 
 weights = [158.0, 164.2, 160.3, 159.9, 162.1, 164.6,
-169.6, 167.4, 166.4, 171.0, 171.2, 172.6]
+           169.6, 167.4, 166.4, 171.0, 171.2, 172.6]
+weights_actual = [160.0, 161.0, 162.0, 163.0, 164.0,165.0,
+                  166.0, 167.0, 168.0, 169.0, 170.0, 171.0]
 
 # APPLY g-h FILTER to GIVEN DATA ----------------------------------------------
-data = g_h_filter(data=weights, x_0=160., dx=1., g=4./10, h=6./10, dt=1.)
+data = g_h_filter(data=weights, x_0=160., dx=1., g=6./10, h=2./3, dt=1.)
 
 print(weights)
 print("")
 print(data)
 
+plt.style.use('classic')
 plt.grid(color ='k', linestyle='--')
 plt.plot(weights, 'o')
 plt.plot(data)
+plt.plot(weights_actual, '--')
 plt.show()
